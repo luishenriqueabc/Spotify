7d9c9979da45f80b76b4f08429c3870514c34676
luishenriqueabc <thais.liana2011@gmail.com>
2023-04-25 14:12:33 -0300

    Header Vertical

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import spotify from '../assets/spotify.png';
 import {RiHome7Fill} from 'react-icons/ri';
 import {FaSearch} from 'react-icons/fa';
 import {BiBlanket} from 'react-icons/bi';
+import {BsPlusSquareFill} from 'react-icons/bs';
+import {BsFillSuitHeartFill} from 'react-icons/bs';
 import './Header.css';
 
 const Header = () =>{
@@ -11,10 +13,16 @@ const Header = () =>{
                 <div className="Logo">
                     <img src= {spotify} alt="Logo" width="125px"/>
                     <ul className="Listas">
-                        <li > <RiHome7Fill size={22}/>  Inicio</li>
-                        <li> <FaSearch   size={22}/>   Buscar</li>
-                        <li> <BiBlanket size={22} />   Sua Biblioteca</li>
+                        <li > <RiHome7Fill size={22}  style={{paddingRight:"20px"}}/>  Inicio</li>
+                        <li> <FaSearch size={22}  style={{paddingRight:"20px"}} />  Buscar  </li>
+                        <li> <BiBlanket size={22}  style={{paddingRight:"20px"}} />   Sua Biblioteca</li>
+                    {/* Espaçamento e criação de playlist */}
+                        <li> <BsPlusSquareFill size={22}  style={{paddingRight:"20px", paddingTop:"30px"}} />  Criar Playlist </li>
+                        <li> <BsFillSuitHeartFill size={22}  style={{paddingRight:"20px"}} />   Musicas Curt...</li>
                     </ul>
+                </div>
+                <div className="Cokies">
+                    <p> Cookies</p>
                 </div>
             </div>
         </>
