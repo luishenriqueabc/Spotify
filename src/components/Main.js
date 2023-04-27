866a9306871070a7ca590a7da05ac3c8a1b00571
luishenriqueabc <thais.liana2011@gmail.com>
2023-04-27 14:39:13 -0300

    Ajustes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import './Main.css';
-import CardImg from '../assets/cardimg.png'
+import CardImg from '../assets/cardimg.jpeg'
+import {BsPlayCircleFill} from 'react-icons/bs'
+
 
 const Main = () =>{
     return(
@@ -11,6 +13,9 @@ const Main = () =>{
             <div className="Cards">
                 <div className="Card">
                     <img src={CardImg} alt="Card" width="180px" height={200}/>
+                    <div className="Icone">
+                        <BsPlayCircleFill size={50} style={{color:"green"}}/>
+                    </div>
                     <h1>Nome</h1>
                     <h3>Descriçao sobre as músicas..</h3>
                 </div>
@@ -90,6 +95,7 @@ const Main = () =>{
                     <h3>Descriçao sobre as músicas..</h3>  
                 </div>
             </div>
+            <hr className="Line"></hr>
         </div>
     
     );
